74b896f299d733fbb5c31ada948c7a99fcdee69a
TheSilvaProf <fabio01@protonmail.ch>
2023-02-02 17:39:59 -0500

    Exer. arrays

---
+++ exer-array.rb
@@ -0,0 +1,8 @@
+alunos = ["Andre", "Sophia", "Laura"]
+notas = [5, 6, 8]
+alunos << "Paulo"
+# puts alunos
+notas << 7.5
+# puts notas
+puts "Andre tirou #{notas[0]}, Sofia tirou #{notas[1]}, Paulo tirou #{notas[3]} e Laura tirou #{notas[2]}"
+puts "Esta turma tem #{alunos.length()} alunos."
---
+++ str_com_input.rb
@@ -0,0 +1,5 @@
+puts ("Digite o seu nome: ")
+nome = gets.chomp()
+
+puts ("Meu nome eh #{nome}")
+puts ("Meu nome tem #{nome.length().to_s()} letras.")
