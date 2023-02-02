fa68299f4f4a87efa757f567dcf8f512ea182b4e
TheSilvaProf <fabio01@protonmail.ch>
2023-02-02 08:41:17 -0500

    Exercicios de string TREINA-DEV

---
+++ strings.rb
@@ -0,0 +1,12 @@
+puts ("Combinando" + "Strings")
+
+puts ("CampusCode".length())
+puts ("CampusCode".downcase())
+puts ("CampusCode".upcase())
+
+nome = "max"
+idade = "30"
+puts ("Meu nome eh " +nome + "e minha idade eh " + idade + "anos")
+puts ("Meu nome eh #{nome} e minha idade eh #{idade} anos")
+
+puts ("Meu nome tem #{nome.length().to_s()} caracteres.")
