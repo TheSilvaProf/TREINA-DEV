831eb7902d2374d3173c98549c23ba671d4b66e0
TheSilvaProf <fabio01@protonmail.ch>
2023-02-02 18:07:06 -0500

    Exer. hash treina dev

---
+++ exer-hash.rb
@@ -0,0 +1,7 @@
+alunos = [
+    { nome: 'Alberto', nota: 7, disciplina: 'Artes' }, { nome: 'Alana', nota: 10, disciplina: 'Ed. Fis.' }, { nome: 'Bastiao', nota: 5, disciplina: 'Ciencias' }
+
+]
+
+
+puts "O aluno #{alunos[0][:nome]} ficou com a nota #{alunos[0][:nota]} em #{alunos[0][:disciplina]}."
